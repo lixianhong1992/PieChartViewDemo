44a1d91ced0e441cc8ea8e2427edabf6a48e3cb5
lixianhong1992 <lixianhong.1992@163.com>
2017-06-06 21:56:54 +0800

    初始化工程

---
+++ PieChartViewDemo/PieChartViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PieChartViewDemo
+//
+//  Created by 李献红 on 17/6/6.
+//  Copyright © 2017年 李献红. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
